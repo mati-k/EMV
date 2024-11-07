45f908051305801f1d87cce1683af7beb11d698a
mati-k <matik.github@gmail.com>
2024-11-07 16:57:25 +0100

    feat: better saved image quality

--- ViewModels/MissionDetailsViewModel.cs
+++ ViewModels/MissionDetailsViewModel.cs
@@ -78,14 +78,7 @@ namespace EMV.ViewModels
 
             RenderTargetBitmap result = new RenderTargetBitmap((int)size.Width, (int)size.Height, 96, 96, PixelFormats.Pbgra32);
 
-            DrawingVisual drawingvisual = new DrawingVisual();
-            using (DrawingContext context = drawingvisual.RenderOpen())
-            {
-                context.DrawRectangle(new VisualBrush(view), null, new Rect(new Point(), size));
-                context.Close();
-            }
-
-            result.Render(drawingvisual);
+            result.Render(view);
             return result;
         }
 
